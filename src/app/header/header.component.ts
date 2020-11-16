5e3da68e9b592584c5774e2e8424dda80a599ef2
dashuai009 <1525836294@qq.com>
2020-11-16 11:57:56 +0800

    header部分

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit ,NgZone,Renderer2} from '@angular/core';
+import { Router } from '@angular/router';
+import {fromEvent,Subscription }from 'rxjs';
+import { I18nService } from 'ng-devui/i18n';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  logoSrc = '../../../favicon.ico';
+  clickSub: Subscription = new Subscription();
+
+  constructor(private renderer2: Renderer2, private ngZone: NgZone, private router: Router, private i18n: I18nService) { 
+    const oldHandler = this.router.errorHandler;
+    this.router.errorHandler =  (err: any) => {
+      // 加载失败的时候刷新重试一次
+      if (err.stack && err.stack.indexOf('Error: Loading chunk') >= 0) {
+        if (localStorage.getItem('lastChunkError') !== err.stack) {
+          localStorage.setItem('lastChunkError', err.stack);
+          window.location.reload();
+        } else {
+          console.error(`We really don't find the chunk...`);
+        }
+      }
+      oldHandler(err);
+    };
+  }
+
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const headerMenu = document.querySelector('#headerMenu');
+      const headerNode = headerMenu.parentNode;
+      const containerNode = document.querySelector('.app-container');
+      this.clickSub.add(fromEvent(headerMenu, 'click').subscribe(e => {
+        if (headerMenu.classList.contains('active')) {
+          this.renderer2.removeClass(headerMenu, 'active');
+          this.renderer2.removeClass(headerNode, 'active');
+        } else {
+          this.renderer2.addClass(headerMenu, 'active');
+          this.renderer2.addClass(headerNode, 'active');
+        }
+      }));
+      this.clickSub.add(fromEvent(containerNode, 'click').subscribe(e => {
+        if (headerMenu.classList.contains('active') && !(<any>headerNode).contains(e.target)) {
+          this.renderer2.removeClass(headerMenu, 'active');
+          this.renderer2.removeClass(headerNode, 'active');
+        }
+      }));
+    });
+    
+  }
+
+}
