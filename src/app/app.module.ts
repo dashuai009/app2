070e2e40bcb2784d5b89351c2e2b8015c804dd33
dashuai009 <1525836294@qq.com>
2020-11-16 11:52:37 +0800

    加上一些要用的组件模块啥的。。。。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// DevUI部分组件依赖angular动画，需要引入animations模块
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DevUIModule } from 'ng-devui';
+
+
+import { ThemePickerModule } from './theme-picker/theme-picker.module';
+import { HeaderComponent } from './header/header.component';
+import { AsideAndContentComponent } from './aside-and-content/aside-and-content.component';
+import { FootComponent } from './foot/foot.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MineSweeperComponent } from './mine-sweeper/mine-sweeper.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideAndContentComponent,
+    FootComponent,
+    MineSweeperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DevUIModule,
+    ThemePickerModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
